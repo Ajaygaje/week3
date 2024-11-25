83b5f56f902cb44cf4101a380c1567747416eff9
Ajaygaje <ajaygaje313@gmail.com>
2024-11-25 23:16:49 +0530

    c

--- Prime.java
+++ Prime.java
@@ -1,34 +1,18 @@
-import java.util.Scanner;
-
 public class Prime {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int num = 29; // Example number to check
+        boolean isPrime = true;
 
-        System.out.print("Enter a number to check if it's prime: ");
-        int num = scanner.nextInt();
-
-        if (isPrime(num)) {
-            System.out.println(num + " is a prime number.");
-        } else {
-            System.out.println(num + " is not a prime number.");
-        }
-
-        scanner.close();
-    }
-
-    public static boolean isPrime(int num) {
-        // Numbers less than or equal to 1 are not prime
-        if (num <= 1) {
-            return false;
-        }
-
-        // Check for divisors from 2 to the square root of the number
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i <= num / 2; i++) {
             if (num % i == 0) {
-                return false; // Not prime if divisible
+                isPrime = false;
+                break;
             }
         }
 
-        return true; // Prime if no divisors found
+        if (isPrime)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
     }
 }
